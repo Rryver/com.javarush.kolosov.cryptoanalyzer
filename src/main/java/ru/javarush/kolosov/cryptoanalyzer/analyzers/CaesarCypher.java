8aa54c4d7377c722088c793139702d360476b2bc
Александр Колосов <kolosov.a@red-promo.ru>
2024-10-05 12:07:37 +0300

    save

--- src/main/java/ru/javarush/kolosov/cryptoanalyzer/analyzers/CaesarCypher.java
+++ src/main/java/ru/javarush/kolosov/cryptoanalyzer/analyzers/CaesarCypher.java
@@ -2,22 +2,34 @@ package ru.javarush.kolosov.cryptoanalyzer.analyzers;
 
 import ru.javarush.kolosov.cryptoanalyzer.analyzers.alphabets.Alphabet;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class CaesarCypher {
 
-    private final Map<Character, Character> alphabet;
+    private final Map<Character, Character> alphabet = new HashMap<>();
 
     private final int key;
 
     public CaesarCypher(int key, char[]... alphabets) {
         this.key = key;
-
         char[] alphabet = Alphabet.merge(alphabets);
+        prepareAlphabet(key, alphabet);
+        System.out.println(this.alphabet.toString());
+    }
 
+    // 0 1 2 3 4 5
+    // key = 2
+    // 2 3 4 5 0 1
+    private void prepareAlphabet(int key, char[] alphabet) {
+        for (int i = 0; i < alphabet.length - key; i++) {
+            this.alphabet.put(alphabet[i], alphabet[i + key]);
+        }
 
-
-        this.alphabet =
+        int startFrom = alphabet.length - key;
+        for (int i = startFrom; i < alphabet.length; i++) {
+            this.alphabet.put(alphabet[i], alphabet[i - startFrom]);
+        }
     }
 
     public String encode(String text) {
@@ -36,6 +48,6 @@ public class CaesarCypher {
 
 
     private char encodeSymbol(char symbol) {
-
+        return alphabet.containsKey(symbol) ? alphabet.;
     }
 }
